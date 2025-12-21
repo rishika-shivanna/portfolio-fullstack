7da74411e1f969bd0121fa424b0c6c740d421e6d
Rishika Shivanna <rishikashivanna@Mac-169.lan>
2025-12-21 17:44:23 -0500

    Add complete project structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
-      <main className="pt-24"> {/* was pt-20 */}
+      <main className="pt-16.5"> {/* was pt-20 */}
         <Home />
       </main>
     </div>
